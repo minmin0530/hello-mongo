38493c0dc0098d13ff3de49bf25eeb4a0eba9b85
minmin0530 <izumiyoshiki@icloud.com>
2023-02-28 05:55:40 +0900

    third commit

--- main.ts
+++ main.ts
@@ -3,40 +3,54 @@ import {
     Router,
     Status,
   } from "https://deno.land/x/oak@v10.0.0/mod.ts";
-  import { Bson, MongoClient } from "https://deno.land/x/mongo@v0.29.3/mod.ts";
+//  import { Bson, MongoClient } from "https://deno.land/x/mongo@v0.29.3/mod.ts";
   import { timeAgo } from "https://deno.land/x/time_ago@v1/mod.ts";
   
   import view from "./view.ts";
   
   const MONGO_URI = Deno.env.get("MONGO_URI");
   if (!MONGO_URI) throw new Error("MONGO_URI not found");
-  
+
+  import { DataTypes, Model, Database, MongoDBConnector } from 'https://deno.land/x/denodb/mod.ts';
+const connector = new MongoDBConnector({
+  uri: MONGO_URI,
+  database: 'posts',
+});
+
+const db = new Database(connector);
+
+
+
+
   const deploymentTime = Date.now();
   
   const app = new Application();
   const router = new Router();
   
-  const client = new MongoClient();
-  try {
-    await client.connect(MONGO_URI);
-  } catch (err) {
-    console.error("Error connecting to MongoDB", err);
-    throw err;
+//   const client = new MongoClient();
+//   try {
+//     await client.connect(MONGO_URI);
+//   } catch (err) {
+//     console.error("Error connecting to MongoDB", err);
+//     throw err;
+//   }
+  
+  class Post extends Model {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      };
+    title: DataTypes.STRING;
   }
   
-  interface Post {
-    _id: Bson.ObjectId;
-    title: string;
-  }
-  
-  const collection = client.database().collection<Post>("posts");
+  const collection = db.collection<Post>("posts");
   
   router.get("/", async (ctx) => {
     try {
       const posts = await collection
         .find({}, { noCursorTimeout: false })
         .sort({
-          _id: -1,
+          id: -1,
         })
         .map((post) => ({
           ...post,
@@ -59,10 +73,8 @@ import {
     const title = body.title;
     if (!title) ctx.throw(Status.BadRequest, "Title is required");
   
-    const post = {
-      _id: new Bson.ObjectId(),
-      title,
-    };
+    const post = new Post();
+    post.title = title;
   
     await collection.insertOne(post);
   
